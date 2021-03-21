d9e3f5705aa52a034fec5eaaedc286d24babedd0
akrupnova <krupnova.anna83@gmail.com>
2021-03-20 21:27:20 -0700

    second commit

--- test/specs/example.e2e.js
+++ test/specs/example.e2e.js
@@ -1,15 +1,39 @@
 import LoginPage from  '../pageobjects/login.page';
-import SecurePage from '../pageobjects/secure.page';
+import ProfilePage from  '../pageobjects/profile.page';
 
-describe('My Login application', () => {
-    it('should login with valid credentials', () => {
+describe('Auth', () => {
+    beforeEach(() => {
         LoginPage.open();
-
-        LoginPage.login('tomsmith', 'SuperSecretPassword!');
-        expect(SecurePage.flashAlert).toBeExisting();
-        expect(SecurePage.flashAlert).toHaveTextContaining(
-            'You logged into a secure area!');
     });
+
+    afterEach(() => {
+        browser.execute('window.localStorage.clear()');
+    });
+
+    it('user logs in with valid data', () => {
+        LoginPage.setLogin('anna.krupnovaa@gmail.com');
+        LoginPage.setPassword('123zxc!!');
+        LoginPage.clickSubmitButton();
+        ProfilePage.isOpen();
+    });
+
+     it('Button Submit is disabled when entering nothing', () => {
+        LoginPage.submitButtonIsDisabled();
+     });
+
+    it('error message with invalid email', () => {
+        LoginPage.setLogin('aaa@example.com');
+        LoginPage.invalidEmail();
+    });
+
+    it('error message with invalid email and password', () => {
+        LoginPage.setLogin('aaa@example.com');
+        LoginPage.setPassword('123zx');
+        LoginPage.clickSubmitButton();
+        LoginPage.errorToastAppeared();
+    });
+
+
 });
 
 
